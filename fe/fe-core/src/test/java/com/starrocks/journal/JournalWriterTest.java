1755e67de7777813050ea3a2ed1cd70c4e94d0f4
gengjun-git <gengjun@starrocks.com>
2023-03-17 17:07:21 +0800

    ignore (#19727)
    Ignore waste memory UT
    Signed-off-by: gengjun-git <gengjun@starrocks.com>
    (cherry picked from commit dbc4fced0b1702f880531cac5e86ae7bb3c9bb28)

--- fe/fe-core/src/test/java/com/starrocks/journal/JournalWriterTest.java
+++ fe/fe-core/src/test/java/com/starrocks/journal/JournalWriterTest.java
@@ -25,6 +25,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -133,6 +134,8 @@ public class JournalWriterTest {
         Assert.assertEquals(1, journalQueue.size());
     }
 
+    // TODO: this ut wastes too much memory
+    @Ignore
     @Test
     public void testTooManyLogs() throws Exception {
         Config.metadata_journal_max_batch_cnt = 2;
